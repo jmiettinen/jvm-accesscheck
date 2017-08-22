e0d1b960631e5cad169b10d8da304a681fd3ca52
Jarkko Miettinen <jarkko.miettinen@relex.fi>
2017-08-22 13:34:52 +0300

    This works, but is not scalable

--- src/main/java/fi/relex/Check.java
+++ src/main/java/fi/relex/Check.java
@@ -2,17 +2,9 @@ package fi.relex;
 
 import java.lang.reflect.Constructor;
 
-/**
- * Hello world!
- */
 public final class Check {
-    private Check() {
-    }
+    private Check() {}
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
     public static void main(String[] args) throws ClassNotFoundException {
         final String className = "com.sun.management.internal.HotSpotThreadImpl";
         Class<?> klass = Class.forName(className);
