8f99082dd75289f35582845503bde5486a784c59
Jarkko Miettinen <jarkko.miettinen@relex.fi>
2017-08-22 13:18:33 +0300

    Print the used switches

--- run.sh
+++ run.sh
@@ -3,6 +3,10 @@
 
 script_dir="$(cd $(dirname $0) && pwd -P)"
 additional_switches="$(tr '\n' ' ' < ${script_dir}/command_line_switches.cfg)"
-echo ${additional_switches}
+
+java -version
+echo ""
+echo "Used switches: ${additional_switches}"
+echo ""
 
 java -cp "${script_dir}/target/accesscheck.jar" ${additional_switches} fi.relex.Check
